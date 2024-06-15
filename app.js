9773ae21284b58db34302c4daf9638e964618aac
TomOkafor <59459410+Tom-Okafor@users.noreply.github.com>
2024-06-15 23:54:00 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,24 @@
 (() => {
     "use strict";
-    console.log("ready!")
+
+    function handleMenuToggleClickEvent() {
+        const MENU_BUTTON = document.querySelector(".menu-holder");
+
+        const MENU_BUTTON_LINES =
+            document.querySelectorAll(".menu-holder > div");
+
+        const MENU = document.querySelector(".menu");
+
+        const MENU_ITEMS = document.querySelectorAll(".menu-item");
+
+        function handleButtonAnimationForMenuOpen() {
+            MENU_BUTTON.addEventListener("click", event => {
+                const MENU_BTN_CLASSNAME = MENU_BUTTON.getAttribute("class");
+                MENU_BUTTON.classList.add(`${MENU_BTN_CLASSNAME}Too`);
+            });
+        }
+        handleButtonAnimationForMenuOpen();
+    }
+
+    handleMenuToggleClickEvent();
 })();
